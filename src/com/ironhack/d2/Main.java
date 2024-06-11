98db052880231246122ad045460b8f58ca1850a9
Tica506 <slentz3@wgu.edu>
2024-06-11 17:58:20 -0400

    lab completed

--- src/com/ironhack/d2/Main.java
+++ src/com/ironhack/d2/Main.java
@@ -3,33 +3,41 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("week 1 day 2 lesson");
-        //ARRAY
-        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday" , "Saturday", "Sunday"};
-    System.out.println(days);
+        System.out.println("Week 1 day 2 lessons");
+
+//        ARRAYS
+
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println(days);
         System.out.println(Arrays.toString(days));
         System.out.println(days[3]);
+        System.out.println(days.length);
         System.out.println(days[days.length - 1]);
 
-        String[] subjects = new String [5];
-        subjects[1] = "Math";
-        subjects[2] = "Language";
-        subjects[3] = "histoy";
-        subjects[4] = "Chemistry";
+        String[] subjectArray = createSubjectsArray();
+        System.out.println(Arrays.toString(subjectArray));
 
-        String[] SubjectArray = createSubjectArray();
+    // LOOPS
+        for ( int i = 0; i < 10; i++) {
 
-    System.out.println(Arrays.toString(subjects));
+            System.out.println(i);
+        }
+        for (int i = 0; i < subjectArray.length; i++){
+            System.out.println(i + ":" + subjectArray[i]);
+        }
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
 
+            }
+        }
     }
 
-
-
-    public static String[] createSubjectArray() {
-        String[] subjects = new String [5];
-        subjects[1] = "Math";
-        subjects[2] = "Language";
-        subjects[3] = "histoy";
+    public static String[] createSubjectsArray() {
+        String[] subjects = new String[5];
+        subjects[0] = "Math";
+        subjects[1] = "Language";
+        subjects[2] = "History";
+        subjects[3] = "Biology";
         subjects[4] = "Chemistry";
 
         return subjects;
